342b3b918d0d395ff8d1432d16975bcab654ccd7
FurryAcetylCoA <a@a.com>
2021-01-21 23:00:16 +0800

    流水灯实验

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stc\STC89C5xRC.H>
-
+#include <intrins.h>
+void Delay100ms();
 void main(){
- P10=0;
+	unsigned char tP0=0xFC;
+	//P26=0;
+	do{
+		P1=tP0;
+	  Delay100ms();
+		tP0=_crol_(tP0,1);
+	}while(1);
+	
+}
+void Delay100ms()		//@11.0592MHz
+{
+	unsigned char i, j;
+
+	i = 180;
+	j = 10;
+	do
+	{
+		while (--j);
+	} while (--i);
 }
