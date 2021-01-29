4ae0e6024a487fa357da1098f1b8f3217b7d1d52
a <coa>
2021-01-29 13:28:28 +0800

    增加管脚名

--- GtxTX1C.c
+++ GtxTX1C.c
@@ -1,7 +1,11 @@
 #include "GtxTX1C.h"
 
 uint8c SegmentDisplay7CC[]={0x3F,0x06,0x5B,0x4F,0x66,0X6D,0X7D,0X07,0X7F,0X6F};
- 
+
+
+
+
+
 void Delay()		//@11.0592MHz
 {//1s
 	unsigned char i, j, k;
@@ -15,6 +19,18 @@ void Delay()		//@11.0592MHz
 		{
 			while (--k);
 		} while (--j);
+	} while (--i);
+}
+
+void Delay10ms()		//@11.0592MHz
+{
+	unsigned char i, j;
+
+	i = 108;
+	j = 145;
+	do
+	{
+		while (--j);
 	} while (--i);
 }
 
@@ -38,3 +54,5 @@ void SegDisplay(uint32 num){	//控制数码管显示数字,右对齐
 		num=num/10;
 	}while(num!=0&&ii--!=0);
 }
+
+
--- GtxTX1C.h
+++ GtxTX1C.h
@@ -26,13 +26,60 @@ typedef signed char   int8;
 typedef signed int    int16;
 typedef signed long   int32;
 
+typedef bit           bool;
+#define true          1;
+#define false         0;
+
+
+//管脚
+sbit DB0 = P1^0;
+sbit DB1 = P1^1;
+sbit DB2 = P1^2;
+sbit DB3 = P1^3;
+sbit DB4 = P1^4;
+sbit DB5 = P1^5;
+sbit DB6 = P1^6;
+sbit DB7 = P1^7;
+
+sbit D0  = P0^0;
+sbit D1  = P0^1;
+sbit D2  = P0^2;
+sbit D3  = P0^3;
+sbit D4  = P0^4;
+sbit D5  = P0^5;
+sbit D6  = P0^6;
+sbit D7  = P0^7;
+
+sbit DAC_CS = P3^2;
+
+sbit LCD_EN = P3^4;
+sbit LCD_RS = P3^5;
+
+
+sbit SEG_WLE  = P2^7;
+sbit SEG_DLE  = P2^6;
+sbit LED_LE   = P2^5;
+
+sbit BUZZER   = P2^3;
+sbit DS18B20  = P2^2;
+sbit C02_SCL  = P2^1;
+sbit C02_SDA  = P2^0;
 
 //码表
 extern uint8c SegmentDisplay7CC[10];
+
 
 //工具函数
 
 void SingleSegDisplay(uint8 which,uint8 num);//控制某一个数码管(0-5),显示数字
 void SegDisplay(uint32 num);         //控制数码管显示数字
+void Delay10ms();
+void Delay();
 
+/*#define QueryKeyDown(which,payload) do{  \
+ /*查询某个管脚是否有按键按下*/	/*   if(which==0){\
+																		Delay10ms();}
+	*/
+	
+	
 #endif //__GtxTX1C_h__
