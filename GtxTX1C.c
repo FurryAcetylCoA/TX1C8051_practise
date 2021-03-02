81b92ea11c3ef64eefc82638db55ce3212bc875b
FurryAcetylCoA <NA@NA.com>
2021-03-02 14:09:45 +0800

    调整代码格式和排版

--- GtxTX1C.c
+++ GtxTX1C.c
@@ -1,15 +1,11 @@
 #include "GtxTX1C.h"
 
+
 uint8c SegmentDisplay7CC[]={0x3F,0x06,0x5B,0x4F,0x66,0X6D,0X7D,0X07,0X7F,0X6F};
 
 
-
-
-
-void Delay()		//@11.0592MHz
-{//0.8s
+void Delay(){//0.8s@11.0592MHz
 unsigned char i, j, k;
-
 	_nop_();
 	i = 6;
 	j = 155;
@@ -36,7 +32,6 @@ void Delay10ms()		//@11.0592MHz
 }
 
 void SingleSegDisplay(uint8 which,uint8 num){
-	
 	P26=0;
 	P27=0;
 	P0=0xFF^(1<<which);
@@ -45,7 +40,6 @@ void SingleSegDisplay(uint8 which,uint8 num){
 	P0=SegmentDisplay7CC[num];
 	P26=1;
 	P26=0;
-
 }
 
 void SegDisplay(uint32 num){	//控制数码管显示数字,右对齐
@@ -53,7 +47,7 @@ void SegDisplay(uint32 num){	//控制数码管显示数字,右对齐
 	do{
 		SingleSegDisplay(ii,num%10);
 		num=num/10;
-	}while(num!=0&&ii--!=0);
+	}while(num!=0&&ii--!=0);	
 }
 
 void Latch573(enum LatchSelectEnum which,uint8 aData){   //控制锁存器
@@ -62,7 +56,5 @@ void Latch573(enum LatchSelectEnum which,uint8 aData){   //控制锁存器
 	P0=aData;		 //写入总线
 	P2=P2|which;	 //使能对应的锁存器
 	P2=P2^which;
-
-	//P0=tempP0backup;
 }
 
